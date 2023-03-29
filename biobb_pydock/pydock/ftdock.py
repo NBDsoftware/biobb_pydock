eaaf8435e76204c2557cbd3cddc567e8990b1825
PabloNA97 <p.navarro.acero@gmail.com>
2023-03-29 18:06:25 +0200

    Added ftdock module

--- biobb_pydock/pydock/ftdock.py
+++ biobb_pydock/pydock/ftdock.py
@@ -2,26 +2,28 @@
 
 """Module containing the Ftdock class and the command line interface."""
 import argparse
+from pathlib import Path
 from biobb_common.generic.biobb_object import BiobbObject
 from biobb_common.configuration import  settings
-from biobb_common.tools import file_utils as fu
 from biobb_common.tools.file_utils import launchlogger
-
+from biobb_pydock.pydock.common import rename_files, copy_files
 
 # 1. Rename class as required
 class Ftdock(BiobbObject):
     """
     | biobb_pydock FtDock
-    | Wrapper class for the `pyDock ftdock <https://life.bsc.es/pid/pydock/doc/tutorial.html#ftdock-process>`_ module.
+    | Wrapper class for the `pyDock ftdock <https://life.bsc.es/pid/pydock/doc/tutorial.html#sampling-using-fast-fourier-transform-fft-methods>`_ module
+    | and the `pyDock rotftdock <https://life.bsc.es/pid/pydock/doc/tutorial.html#sampling-using-fast-fourier-transform-fft-methods>`_ module.
     | The pyDock ftdock module is used to generate docking positions using a Fast Fourier Transform algorithm (FFT).
+    | The pyDock rotftdock module is used to generate the transformation matrix for all the docking poses.
 
     Args:
         input_receptor_path (str): Prepared receptor PDB file with pydock setup (the largest of the two proteins). File type: input. `Sample file <>`_. Accepted formats: pdb (edam:format_1476).
         input_lig_path (str): Prepared ligand PDB file with pydock setup (will be rotated and translated). File type: input. `Sample file <>`_. Accepted formats: pdb (edam:format_1476).
-        output_ftdock_path (str) (Optional): Receptor PDB file with the correct chain name adapted for pyDock ftdock or zdock. docking_name_rec.pdb by default. File type: output. `Sample file <>`_. Accepted formats: pdb (edam:format_1476).
+        output_ftdock_path (str): File with docking poses expressed as a translation vector and 3 rotation angles. File type: output. `Sample file <>`_. Accepted formats:  (edam:).
+        output_rot_path (str): File containing the transformation matrix for all the docking poses. File type: output. `Sample file <>`_. Accepted formats:  (edam:).
         properties (dic):
             * **docking_name** (*str*) - ("docking_name") Name for the docking.
-
             * **binary_path** (*str*) - ("pyDock3") Path to the pyDock executable binary.
             * **remove_tmp** (*bool*) - (True) [WF property] Remove temporal files.
             * **restart** (*bool*) - (False) [WF property] Do not execute if output files exist.
@@ -33,21 +35,22 @@ class Ftdock(BiobbObject):
             * **container_shell_path** (*str*) - ("/bin/bash") Path to the binary executable of the container shell.
 
     Examples:
-        This is a use example of how to use the building block from Python::
+        This is a use example of how to use the building block from Python:
 
             from biobb_pydock.pydock.ftdock import ftdock
 
             prop = { 
                 'docking_name': 'docking_name'}
 
-            ftdock(input_receptor_path='/path/to/my/Receptor/docking_name_rec.pdb',
-                  input_lig_path='/path/to/my/Ligand/docking_name_lig.pdb',
-                  output_ftdock_path='/path/to/ ',
+            ftdock(input_receptor_path='/path/to/my/Receptor/prepared_receptor.pdb',
+                  input_lig_path='/path/to/my/Ligand/prepared_ligand.pdb',
+                  output_ftdock_path='/path/to/new/docking/positions/dock_poses.ftdock',
+                  output_rot_path='/path/to/poses_matrix.rot',
                   properties=prop)
 
     Info:
         * wrapped_software:
-            * name: pyDock ftdock
+            * name: pyDock ftdock and pyDock rotftdock
             * version: >=3.6.1
             * license: 
         * ontology:
@@ -58,28 +61,30 @@ class Ftdock(BiobbObject):
 
     # Adapt input and output file paths as required. Include all files, even optional ones
     def __init__(self, input_receptor_path: str, input_lig_path: str, output_ftdock_path: str,
-                 properties: dict = None, **kwargs) -> None:
+                 output_rot_path: str, properties: dict = None, **kwargs) -> None:
         properties = properties or {}
 
         # Call parent class constructor
         super().__init__(properties)
         self.locals_var_dict = locals().copy()
 
-        # Input/Output files
+        # Properties common to all PyDock BB
+        self.docking_name = properties.get('docking_name', 'docking_name')
+        self.binary_path = properties.get('binary_path', 'pydock3') 
+
+        # Save external user-defined paths in properties (only those that need "docking_name" in their file name)
+        self.external_input_paths = {'input_receptor_path': input_receptor_path, 'input_lig_path': input_lig_path}
+        self.external_output_paths = {'output_ftdock_path': output_ftdock_path, 'output_rot_path': output_rot_path}
+
+        # Input/Output files 
         self.io_dict = { 
-            'in': { 'input_receptor_path': input_receptor_path, 'input_lig_path': input_lig_path }, 
-            'out': { 'output_receptor_path': output_receptor_path, 'output_ligand_path': output_ligand_path } 
+            'in': { 'input_receptor_path': f'{self.docking_name}_rec.pdb', 'input_lig_path': f'{self.docking_name}_lig.pdb' }, 
+            'out': { 'output_ftdock_path': f'{self.docking_name}.ftdock', 'output_rot_path': f'{self.docking_name}.rot'} 
         }
 
-        # Properties specific for BB
-        self.docking_name = properties.get('docking_name', 'docking_name')
-        self.receptor_mol = properties.get('receptor_mol', 'A')
-        self.receptor_newmol = properties.get('receptor_newmol', 'A')
-        self.ligand_mol = properties.get('ligand_mol', 'A')
-        self.ligand_newmol = properties.get('ligand_newmol', 'B')
-        self.binary_path = properties.get('binary_path', 'zip')
-        self.properties = properties
-
+        # Copy input files to stage them with correct names
+        copy_files(source_paths = self.external_input_paths, destination_paths = self.io_dict["in"])
+        
         # Check the properties
         self.check_properties(properties)
         # Check the arguments
@@ -93,23 +98,30 @@ class Ftdock(BiobbObject):
         if self.check_restart(): return 0
         self.stage_files()
 
-        # Create command line
-        self.cmd = [self.binary_path, self.properties['docking_name'], 'ftdock']
-        fu.log('Creating command line with instructions and required arguments', self.out_log, self.global_log)
+        # Create ftdock command path: /path/to/inputs + /docking_name
+        if self.container_path:
+            ftdock_path = str(Path(self.container_volume_path).joinpath(self.docking_name)) 
+        else:
+            ftdock_path = str(Path(self.stage_io_dict.get("unique_dir")).joinpath(self.docking_name)) 
 
-        # 8. Uncomment to check the command line 
-        print(' '.join(self.cmd))
+        # Create rotftdock command path: /working_dir + /docking_name
+        rotftdock_path = self.docking_name
+
+        # Create command line
+        self.cmd = [self.binary_path, ftdock_path, 'ftdock', '&&', self.binary_path, rotftdock_path, 'rotftdock']
 
         # Run Biobb block
         self.run_biobb()
 
-        # If output paths are provided, change the output file names?
-
         # Copy files to host
         self.copy_to_host()
 
+        # Rename output files 
+        rename_files(source_paths = self.io_dict["out"], destination_paths = self.external_output_paths)
+
         # Remove temporal files
         self.tmp_files.append(self.stage_io_dict.get("unique_dir"))
+        self.tmp_files.extend(list(self.io_dict['in'].values()))     # Add duplicated input files
         self.remove_tmp_files()
 
         # Check output arguments
@@ -117,25 +129,25 @@ class Ftdock(BiobbObject):
 
         return self.return_code
 
-def ftdock(input_receptor_path: str, input_lig_path: str, output_receptor_path: str, output_ligand_path: str, properties: dict = None, **kwargs) -> int:
+def ftdock(input_receptor_path: str, input_lig_path: str, output_ftdock_path: str, output_rot_path: str, properties: dict = None, **kwargs) -> int:
     """Create :class:`Ftdock <pydock.ftdock.Ftdock>` class and
     execute the :meth:`launch() <pydock.ftdock.Ftdock.launch>` method."""
 
-    return Ftdock(input_receptor_path = input_receptor_path, input_lig_path = input_lig_path, output_receptor_path = output_receptor_path, output_ligand_path = output_ligand_path,
-                 properties = properties, **kwargs).launch()
+    return Ftdock(input_receptor_path = input_receptor_path, input_lig_path = input_lig_path, output_ftdock_path = output_ftdock_path,
+                  output_rot_path = output_rot_path, properties = properties, **kwargs).launch()
 
 def main():
     """Command line execution of this building block. Please check the command line documentation."""
-    parser = argparse.ArgumentParser(description='Wrapper of the pyDock pyDock3 ftdock module.', formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999))
+    parser = argparse.ArgumentParser(description='Wrapper of the pyDock ftdock and rotftdock modules.', formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999))
     parser.add_argument('--config', required=False, help='Configuration file')
 
     # Specific args of each building block
     required_args = parser.add_argument_group('required arguments')
     required_args.add_argument('--input_receptor_path', required=True, help='Receptor PDB file (the largest of the two proteins). Accepted formats: pdb.')
     required_args.add_argument('--input_lig_path', required=True, help='Ligand PDB file (will be rotated and translated). Accepted formats: pdb.')
-    parser.add_argument('--output_receptor_path', required=False, help='Receptor PDB file with the correct chain name adapted for pyDock ftdock or zdock. docking_name_rec.pdb by default. (optional). Accepted formats: pdb.')
-    parser.add_argument('--output_ligand_path', required=False, help='Ligand PDB file with the correct chain name adapted for pyDock ftdock or zdock. docking_name_lig.pdb by default. (optional). Accepted formats: pdb.')
-    
+    required_args.add_argument('--output_ftdock_path', required=True, help='Output ftdock file. Accepted formats: ftdock')
+    required_args.add_argument('--output_rot_path', required=True, help='Output rotftdock file. Accepted formats: rot')
+
     args = parser.parse_args()
     args.config = args.config or "{}"
     properties = settings.ConfReader(config=args.config).get_prop_dic()
@@ -143,8 +155,8 @@ def main():
     # Specific call of each building block
     ftdock(input_receptor_path = args.input_receptor_path
           ,input_lig_path = args.input_lig_path
-          ,output_receptor_path = args.output_receptor_path
-          ,output_ligand_path = args.output_ligand_path
+          ,output_ftdock_path = args.output_ftdock_path
+          ,output_rot_path = args.output_rot_path
           ,properties = properties)
 
 if __name__ == '__main__':
---
+++ biobb_pydock/test/ftdock_test.py
@@ -0,0 +1,24 @@
+from biobb_pydock.pydock.ftdock import ftdock
+
+input_receptor_path = '/shared/scratch/jobs/pnavarro/2023_EUCANSHARE/biobb_pydock/biobb_pydock/test/data/ftdock/prepared_receptor.pdb'
+input_lig_path = '/shared/scratch/jobs/pnavarro/2023_EUCANSHARE/biobb_pydock/biobb_pydock/test/data/ftdock/prepared_ligand.pdb'
+
+container_volume_path = '/data'
+container_working_dir = container_volume_path  # Avoid execution in $HOME, tmp files are created in the working dir
+container_path = 'singularity'
+container_generic_command = 'exec'
+container_image = '/shared/work/NBD_Utilities/pyDock3/3.6.1/nbd_pydock.sif' 
+
+prop = { 
+    'docking_name': 'docking_name',
+    'container_path': container_path,
+    'container_image': container_image,
+    'container_volume_path': container_volume_path,
+    'container_working_dir': container_working_dir,
+    'container_generic_command': container_generic_command}
+   
+ftdock(input_receptor_path=input_receptor_path,
+    input_lig_path=input_lig_path,
+    output_ftdock_path='ftdock_output.ftdock',
+    output_rot_path='rotftdock_output.rot',
+    properties=prop)
